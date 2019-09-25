e8e347d2ef46b57c41b54c91861a93855b5e2dae
samratsauravjaiswal <coolsaurav34@gmail.com>
2019-09-25 12:20:05 +0530

    adi design

--- src/components/Offers.js
+++ src/components/Offers.js
@@ -5,15 +5,19 @@ import account from '../images/acoount.png';
 export default function Offers() {
   return (
     <Fragment>
-      <div className='row' style={ { marginBottom: '0px',paddingBottom: '0px', marginRight:'4rem,'}}>
+      <div className='row' style={{ marginBottom: '0px', paddingBottom: '0px' }}>
         <div
           className=' col-sm-6 col-md-3 pro'
-          style={{ textAlign: 'center', }}>
-          <span className='left-top' style={ {fontSize: '13px'}}>Sign up &amp; get gift voucher worth `1000</span>
+          style={{ textAlign: 'left', marginLeft: '-4.5rem' }}>
+          <span className='left-top' style={{ fontSize: '13px', paddingLeft: '0' }}>
+            Sign up &amp; get gift voucher worth `1000
+          </span>
         </div>
         <div className=' col-md-6'></div>
-        <div className='col-sm-6 col-md-3 pro' style={{ padding: '0.5rem 4.5rem', paddingBottom:'0px'}}>
-          <Link className='login'  href >
+        <div
+          className='col-sm-6 col-md-3 pro'
+          style={{ padding: '0.5rem ', paddingBottom: '0px' }}>
+          <Link className='login' href>
             <img src={account} alt='' />
             <span>&nbsp;&nbsp; Login &nbsp;|&nbsp; Register</span>
           </Link>
