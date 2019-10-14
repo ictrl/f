2bd2920351728b9f7cb1d409f8f40b580b743f76
Aditya Kumar Jha <adityajha126@gmail.com>
2019-10-15 03:38:41 +0530

    canva

--- src/components/CreateYourPrint.js
+++ src/components/CreateYourPrint.js
@@ -1,96 +1,81 @@
-import React, { useContext, Fragment, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { ThemeContext } from "../App";
+import React, { useContext, Fragment, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { ThemeContext } from '../App';
 
 export default function CreateYourPrint() {
-  const context = useContext(ThemeContext);
-  const { productProperty, setProductProperty } = context;
+	const context = useContext(ThemeContext);
+	const { productProperty, setProductProperty } = context;
 
-  return (
-    <Fragment>
-      <div className="row" style={{ marginBottom: "3rem" }}>
-        <div className="col-sm-12 center" style={{ marginBottom: "30px" }}>
-          <h3>
-            <strong style={{ color: "#ff9900" }}>CREATE YOUR OWN CANVAS</strong>
-          </h3>
-          <h4>(Choose the perfect material)</h4>
-        </div>
+	return (
+		<Fragment>
+			<div className="row" style={{ marginBottom: '3rem' }}>
+				<div className="col-sm-12 center" style={{ marginBottom: '30px' }}>
+					<h3>
+						<strong style={{ color: '#ff9900' }}>CREATE YOUR OWN CANVA</strong>
+					</h3>
+					<h4>(Choose the perfect material)</h4>
+				</div>
 
-        <Link
-          to="CanvasPrint"
-          onClick={() => {
-            setProductProperty({ ...productProperty, material: "Canvas" });
-          }}
-        >
-          <div className="col-sm-4">
-            <div className="card center canvas-card">
-              <img
-                src={require("../images/material-card-1.png")}
-                alt="Avatar"
-                style={{ width: "100%" }}
-              />
-              <h4>
-                <b>Canvas</b>
-              </h4>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s,
-              </p>
-            </div>
-          </div>
-        </Link>
+				<Link
+					to="CanvasPrint"
+					onClick={() => {
+						setProductProperty({ ...productProperty, material: 'Canvas' });
+					}}
+				>
+					<div className="col-sm-4">
+						<div className="card center canvas-card">
+							<img src={require('../images/material-card-1.png')} alt="Avatar" style={{ width: '100%' }} />
+							<h4>
+								<b>Canvas</b>
+							</h4>
+							<p>
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+								industry's standard dummy text ever since the 1500s,
+							</p>
+						</div>
+					</div>
+				</Link>
 
-        <Link
-          to="CanvasPrint"
-          onClick={() => {
-            setProductProperty({ ...productProperty, material: "HD Metal" });
-          }}
-        >
-          <div className="col-sm-4">
-            <div className="card center canvas-card">
-              <img
-                src={require("../images/material-card-2.png")}
-                alt="Avatar"
-                style={{ width: "100%" }}
-              />
-              <h4>
-                <b>HD Metal</b>
-              </h4>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s,
-              </p>
-            </div>
-          </div>
-        </Link>
+				<Link
+					to="CanvasPrint"
+					onClick={() => {
+						setProductProperty({ ...productProperty, material: 'HD Metal' });
+					}}
+				>
+					<div className="col-sm-4">
+						<div className="card center canvas-card">
+							<img src={require('../images/material-card-2.png')} alt="Avatar" style={{ width: '100%' }} />
+							<h4>
+								<b>HD Metal</b>
+							</h4>
+							<p>
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+								industry's standard dummy text ever since the 1500s,
+							</p>
+						</div>
+					</div>
+				</Link>
 
-        <Link
-          to="CanvasPrint"
-          onClick={() => {
-            setProductProperty({ ...productProperty, material: "Acrylic" });
-          }}
-        >
-          <div className="col-sm-4">
-            <div className="card center canvas-card">
-              <img
-                src={require("../images/material-card-3.png")}
-                alt="Avatar"
-                style={{ width: "100%" }}
-              />
-              <h4>
-                <b>Acrylic</b>
-              </h4>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s,
-              </p>
-            </div>
-          </div>
-        </Link>
-      </div>
-    </Fragment>
-  );
+				<Link
+					to="CanvasPrint"
+					onClick={() => {
+						setProductProperty({ ...productProperty, material: 'Acrylic' });
+					}}
+				>
+					<div className="col-sm-4">
+						<div className="card center canvas-card">
+							<img src={require('../images/material-card-3.png')} alt="Avatar" style={{ width: '100%' }} />
+							<h4>
+								<b>Acrylic</b>
+							</h4>
+							<p>
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+								industry's standard dummy text ever since the 1500s,
+							</p>
+						</div>
+					</div>
+				</Link>
+			</div>
+		</Fragment>
+	);
 }
